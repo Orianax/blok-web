dcee24006499e8ea4728579b1da745e7b4e642f4
Orianax <118364238+Orianax@users.noreply.github.com>
2022-12-14 21:04:14 +0100

    1
    1

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,25 @@
-// JavaScript Document
-console.log("hi");
+console.log("hi")
+
+var hamburgerMenu = document.querySelector(".hamburgermenu");
+var sluitenMenu = document.querySelector(".sluiten");
+var navigatie = document.querySelector(".onzichtbaar");
+
+var fotoOutfits = document.querySelector(".vieroutfits")
+
+hamburgerMenu.addEventListener("click", toggleMenu)
+sluitenMenu.addEventListener("click", toggleMenu)
+
+console.log("menu")
+
+function toggleMenu() {
+    console.log("menu")
+    navigatie.classList.toggle("tevoorschijn");
+}
+
+
+
+function fotoStraat() {
+    fotoOutfits.src = "./images/domfoto2.png"
+}
+
+fotoOutfits.addEventListener("click", fotoStraat)
